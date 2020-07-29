cac58873ab2ceef23412016461e1847fa6a8d108
Kacper Kondracki <eshake1000@gmail.com>
2020-07-29 10:31:34 +0200

    Added verification service

---
+++ Services/VerificationService.cs
@@ -0,0 +1,30 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsuFriendBot.Services
+{
+    class VerificationService
+    {
+        private readonly DiscordSocketClient _discord;
+        private readonly ILogger _logger;
+
+        public VerificationService(DiscordSocketClient discord, ILogger<VerificationService> logger)
+        {
+            _discord = discord;
+            _logger = logger;
+
+            _discord.UserJoined += UserJoinedAsync;
+        }
+
+        public async Task UserJoinedAsync(SocketGuildUser user)
+        {
+            // TODO: Check database before sending message
+            await user.SendMessageAsync($"Hi {user.Username}! Verify your osu! account to get personalized roles.");
+        }
+    }
+}
