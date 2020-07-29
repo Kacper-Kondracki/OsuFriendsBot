6a157d3f98499331d4c1e953677efc189b294c95
Kacper Kondracki <eshake1000@gmail.com>
2020-07-29 15:04:10 +0200

    Add guild settings database service

---
+++ Models/GuildSettings.cs
@@ -0,0 +1,14 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OsuFriendBot.Models
+{
+    public class GuildSettings
+    {
+        [BsonId]
+        public ulong GuildId { get; set; }
+        public string Prefix { get; set; }
+    }
+}
